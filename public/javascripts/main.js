e515ad2f549489d7f5295d376de3c2596aa6c94e
rjmccluskey <rjmccluskey@gmail.com>
2016-01-03 16:56:21 -0800

    better responsive menu for small screens

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,4 +12,33 @@ $(function(){
         }
 
     }
+
+    if ($('#menu')) {
+        var interval;
+        var $menu = $('#menu');
+        $('.scroll-arrow-left').on('mousedown touchstart', function() {
+            interval = setInterval(function() {
+                $menu.scrollLeft( $menu.scrollLeft() - 4);
+            }, 20);
+        });
+        $('.scroll-arrow-right').on('mousedown touchstart', function() {
+            interval = setInterval(function() {
+                $menu.scrollLeft( $menu.scrollLeft() + 4);
+            }, 20);
+        });
+        $('.scroll-arrow-left,.scroll-arrow-right').on('mouseup mouseleave touchend', function(){
+            clearInterval(interval);
+        });
+
+        var $activeMenuItem = $('.pure-menu-link.active');
+        var $allMenuItems = $('.pure-menu-link');
+        var leftPosition = 0;
+        $allMenuItems.each(function() {
+            if ($(this).text() === $activeMenuItem.text()) {
+                return false;
+            }
+            leftPosition += $(this).outerWidth();
+        });
+        $menu.scrollLeft(leftPosition);
+    }
 });
